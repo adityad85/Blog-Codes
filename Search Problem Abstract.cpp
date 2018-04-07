428b5e2e0e13bd7d4e2788cfa4b2482d26040a42
adityad85 <aditya85d@gmail.com>
2018-04-07 15:19:24 +0530

    done

---
+++ Search Problem Abstract.cpp
@@ -0,0 +1,14 @@
+class SearchProblem{
+    SearchProblem(){
+        //intialize
+    }
+    void getStartState(){
+        //return start state
+    }
+    void isGoalState(){
+        //return true if the goal state is reached
+    }
+    void getSuccessors(){
+        //return list of successors (next states reachable ) from the given state
+    }
+}
